ba59524c2c6b2d1c98fdc94361820e4b889a7364
RitaFurman <margaret_furman@yahoo.com>
2020-08-01 10:50:57 -0500

    @BI_32add.login.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,6 +2,8 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("I want to sleep");
 
-
+        for (int i = 0; i <=100 ; i+=2) {
+            System.out.print(i+" ");
+        }
     }
 }
