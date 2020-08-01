9b3e0a184a88bfad36b89fd1c4a4285745ba1c8d
RitaFurman <68919646+RitaFurman@users.noreply.github.com>
2020-08-01 15:03:57 -0500

    Update LogCall.java

--- src/LogCall.java
+++ src/LogCall.java
@@ -1,5 +1,10 @@
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("Rita");
+        
+        
+        // I added this line from GitHub
+        
+        
     }
 }
